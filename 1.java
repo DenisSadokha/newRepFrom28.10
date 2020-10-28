3d14846d611332b77d5e9bdc8d8ed435ebcbdedc
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-10-28 15:23:29 +0700

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,24 +1,28 @@
-// Можно использовать импорты, например:
-// import java.util.*;
+public class Solution {
+    int res;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
- class Solution {
     public int solution(int[] a) {
+//        Arrays.sort(a);
+//        int res=-1;
+//        for (int i = 1; i<a.length; i++){
+//            if(a[i]-a[i-1]!=1){
+//                res = i+1;
+//                break;
+//            }
+//        }
+//        return res;
+//         }
         Arrays.sort(a);
-        int res=-1;
-
-        for (int i = 1; i<a.length; i++){
-            if(a[i]-a[i-1]!=1){
-                res= i+1;
+        for (int i =0; i<a.length; i++){
+            if(a[i]==i+1){
+            } else {
+                res=i+1;
                 break;
             }
         }
 
-
-
-        return res;
+return res;
     }
+
+
 }
