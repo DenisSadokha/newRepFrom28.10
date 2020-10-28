b3319015f9edc61b99493251dec22d2a3cf99b91
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-10-28 10:32:54 +0700

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,8 +1,24 @@
 // Можно использовать импорты, например:
 // import java.util.*;
 
-class Solution {
-    public int solution(int[] A) {
-        // TODO. Java 8 or Java 11
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+ class Solution {
+    public int solution(int[] a) {
+        Arrays.sort(a);
+        int res=-1;
+
+        for (int i = 1; i<a.length; i++){
+            if(a[i]-a[i-1]!=1){
+                res= i+1;
+                break;
+            }
+        }
+
+
+
+        return res;
     }
 }
