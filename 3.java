9282e2e9197e51c51e544b5b71effa078163f084
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-10-28 12:12:08 +0700

    Update 3.java

--- 3.java
+++ 3.java
@@ -1,8 +1,24 @@
-// Можно использовать импорты, например:
-// import java.util.*;
 
-class Solution {
-    public int solution(int[] A) {
-        // TODO. Java 8 or Java 11
+ class Solution {
+    int res;
+    int min;
+    int lSum=0;
+    int rSum=0;
+
+    public int solution(int[] a) {
+        for (int i =0; i<a.length; i++){
+            min +=a[i];
+        }
+        lSum=min-a[a.length-1];
+        rSum=a[a.length-1];
+        for (int i = a.length-1; i>0; i--){
+            if (Math.abs(lSum-rSum)<min){
+                min=Math.abs(lSum-rSum);
+            }
+            lSum=lSum-a[i-1];
+            rSum=rSum+a[i-1];
+        }
+
+        return min;
     }
 }
